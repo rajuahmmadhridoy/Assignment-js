0fdbd1a331deb5315a0fc6c56450e3ffc327c700
Raju Ahmmad Hridoy <rajuahmmad0187@gmail.com>
2021-01-22 02:01:53 +0600

    add f to F

--- assignment.js
+++ assignment.js
@@ -4,13 +4,13 @@
     ***************************** */
 
     
-function kilometerToMeter(Meter){
-    var oneKilo = 1000;
-    var meter = Meter * oneKilo;
-    return meter;
-}
+// function kilometerToMeter(Meter){
+//     var oneKilo = 1000;
+//     var meter = Meter * oneKilo;
+//     return meter;
+// }
 
-var kilo  = kilometerToMeter(3);
+// var kilo  = kilometerToMeter(3);
 
 
 
@@ -22,21 +22,21 @@ var kilo  = kilometerToMeter(3);
 
 
 
-function budgetCalculator(watch, mobile, laptop) {
+// function budgetCalculator(watch, mobile, laptop) {
 
-    var watchPrice = 50;
-    var mobilePrice = 100;
-    var laptopPrice = 500;
+//     var watchPrice = 50;
+//     var mobilePrice = 100;
+//     var laptopPrice = 500;
 
-    var watchCost = watchPrice * watch;
-    var mobileCost = mobilePrice * mobile;
-    var laptopCost = laptopPrice * laptop;
+//     var watchCost = watchPrice * watch;
+//     var mobileCost = mobilePrice * mobile;
+//     var laptopCost = laptopPrice * laptop;
     
-    var totalCost = watchCost + mobileCost + laptopCost;
+//     var totalCost = watchCost + mobileCost + laptopCost;
 
-    return totalCost;
-}
-var budgetCost = budgetCalculator(20,10,8);
+//     return totalCost;
+// }
+// var budgetCost = budgetCalculator(20,10,8);
 
 
 
@@ -46,39 +46,39 @@ var budgetCost = budgetCalculator(20,10,8);
 
 
 
-function hotelCost(days) {
+// function hotelCost(days) {
 
 
-    if(days <=10) {
-       var totalCost = 100 * days;
-        return totalCost;
-    }
-    else if(days <=20) {
-         var firstCost = 100 * 10;
-         var offer = 20;
-         var remaining = days - 10;
-         var costForOffer = 100 - offer;
-         var secondCost = remaining * costForOffer;
-         var totalCost = firstCost + secondCost;
+//     if(days <=10) {
+//        var totalCost = 100 * days;
+//         return totalCost;
+//     }
+//     else if(days <=20) {
+//          var firstCost = 100 * 10;
+//          var offer = 20;
+//          var remaining = days - 10;
+//          var costForOffer = 100 - offer;
+//          var secondCost = remaining * costForOffer;
+//          var totalCost = firstCost + secondCost;
 
-        return totalCost ;
-    }
-    else {
-         var firstCost = 100 * 10 ;
-         var secondCost = (100-20) * 10;
-         var offer = 50;
-         var costForOffer = 100 - offer;
-         var remaining = days - 20;
-         var thirdCost = remaining * costForOffer;
-         var totalCost = firstCost + secondCost + thirdCost;
+//         return totalCost ;
+//     }
+//     else {
+//          var firstCost = 100 * 10 ;
+//          var secondCost = (100-20) * 10;
+//          var offer = 50;
+//          var costForOffer = 100 - offer;
+//          var remaining = days - 20;
+//          var thirdCost = remaining * costForOffer;
+//          var totalCost = firstCost + secondCost + thirdCost;
         
-        return totalCost;
-    }
+//         return totalCost;
+//     }
 
-}
-var tourDays = 21;
-var totalHotelCost=hotelCost(tourDays);
-console.log(totalHotelCost);
+// }
+// var tourDays = 21;
+// var totalHotelCost=hotelCost(tourDays);
+// console.log(totalHotelCost);
 
 
 
@@ -88,7 +88,7 @@ console.log(totalHotelCost);
     ***************************** */
 
 
-function megaFriend(friends) {
+function megaFriend(Friends) {
 
     var mega = Friends[0];
     for(let i = 0 ; i < Friends.length ; i++) {
